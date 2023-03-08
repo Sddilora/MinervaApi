020c24294bd3d5ebbe45ef0a443f51277530b973
Sddilora <sumeyyedilaradogan@gmail.com>
2023-03-08 03:36:17 +0300

    Key folder deleted,key process is handled directly in create part now

--- Create/create.go
+++ Create/create.go
@@ -1,9 +1,9 @@
 package create
 
 import (
-	key "api/Key"
 	"context"
 	"log"
+	"os"
 	"time"
 
 	"cloud.google.com/go/firestore"
@@ -26,14 +26,17 @@ func NewFireStore() (*firestore.Client, *firestore.CollectionRef, context.Contex
 		ctx     context.Context
 	)
 
-	jsonData := key.Key() //we will make that easier
+	fileContents, err := os.ReadFile("C:/Users/sumey/Desktop/software/Back-End/Minerva/Create/key.json") // Read and store as []byte the key file required for us to access our Firestore database
+	if err != nil {
+		log.Fatalf("Failed Read file: %v", err)
+	}
 
 	ctx = context.Background() // Create a new Firestore client using the Google Application Credentials path
 	opt := option.WithGRPCDialOption(grpc.WithKeepaliveParams(keepalive.ClientParameters{
 		Time: 2 * time.Minute,
 	}))
 
-	appfire, err := firestore.NewClient(ctx, "minerva-95196", option.WithCredentialsJSON(jsonData), opt) //we are creating new firestroe client
+	appfire, err := firestore.NewClient(ctx, "minerva-95196", option.WithCredentialsJSON(fileContents), opt) //we are creating new firestroe client
 	if err != nil {
 		log.Fatalf("Failed to create Firestore client: %v", err)
 	}
