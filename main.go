232110397a2e229f810a0aa13406cb16a9554b59
Sddilora <sumeyyedilaradogan@gmail.com>
2023-03-09 23:47:05 +0300

    fiberApp initiator and listener moved to main.go

--- main.go
+++ main.go
@@ -1,12 +1,48 @@
 package main
 
-import setup "api/Setup"
+import (
+	create "api/Create"
+	requests "api/Requests"
+
+	auth "api/Users"
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	recoverMw "github.com/gofiber/fiber/v2/middleware/recover"
+	gofiberfirebaseauth "github.com/sacsand/gofiber-firebaseauth"
+)
 
 func main() {
 
-	start := setup.Setup() // start is err
+	app := fiber.New()
 
-	if start != nil {
-		panic(start)
-	}
+	//Middlewares
+	app.Use(logger.New())    //records the details of incoming requests when any HTTP request is made. This can be used for purposes such as debugging and performance optimization.
+	app.Use(recoverMw.New()) //catches any errors that may cause the program to crash or interrupt and keep the server running.
+	app.Use(cors.New())      //It helps applications bypass CORS restrictions by providing appropriate responses that allow or deny HTTP requests access to their resources.
+
+	_, _, fireApp := create.NewFireStore()
+
+	app.Post("/signup", auth.CreateUserHandler)
+	app.Post("/signin", auth.SigninHandler)
+
+	// app.Use("/users", auth.CreateUserHandler)
+	app.Use(gofiberfirebaseauth.New(gofiberfirebaseauth.Config{
+		// Firebase Authentication App Object
+		// Mandatory
+		FirebaseApp: fireApp,
+		// Ignore urls array.
+		// Optional. These url will ignore by middleware
+		IgnoreUrls: []string{"GET::/topics", "GET::/topic/researches"},
+	}))
+
+	// Authenticaed Routes.
+	app.Post("/topic", requests.PostTopic)
+	app.Post("/topic/research", requests.Research)
+	// app.Get("/topics" /*,func will be added */)           // Ignore the auth by IgnoreUrls config
+	// app.Get("/topic/researches" /*,func will be added */) // Ignore the auth by IgnoreUrls config
+
+	log.Fatal(app.Listen(":7334"))
 }
