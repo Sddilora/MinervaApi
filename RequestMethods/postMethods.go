dedc74a22b4eb7f88d6c639e9eeb47a5eadcd74a
Sddilora <sumeyyedilaradogan@gmail.com>
2023-03-11 20:40:29 +0300

    post methods moved into this file from main

---
+++ RequestMethods/postMethods.go
@@ -0,0 +1,24 @@
+package methods
+
+import (
+	requests "api/RouteHandlers"
+	auth "api/Users"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func PostMethods(app *fiber.App) {
+
+	//Auth Routes
+	app.Post("/signup", auth.CreateUserHandler)
+	app.Post("/signin", auth.SigninHandler)
+
+	//Save data to Database Routes
+	app.Post("/topic", requests.PostTopicHandler)
+	app.Post("/topic/research", requests.PostResearchHandler)
+
+	//Retrieve data from Database Routes
+	app.Post("/topics", requests.PostTopicsHandler)
+	//app.Post("/topic/researches", requests.PostResearchesHandler)
+
+}
