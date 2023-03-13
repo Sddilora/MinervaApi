7cb6e3860ff92ef5d06ae7837d209923f6879d68
Sddilora <sumeyyedilaradogan@gmail.com>
2023-03-13 19:18:54 +0300

    requests packages names' changed

--- RequestMethods/postMethods.go
+++ RequestMethods/postMethods.go
@@ -1,7 +1,7 @@
 package methods
 
 import (
-	requests "api/RouteHandlers"
+	handlers "api/RouteHandlers"
 	auth "api/Users"
 
 	"github.com/gofiber/fiber/v2"
@@ -10,15 +10,15 @@ import (
 func PostMethods(app *fiber.App) {
 
 	//Auth Routes
-	app.Post("/signup", auth.CreateUserHandler)
-	app.Post("/signin", auth.SigninHandler)
+	app.Post("/login", auth.CreateUserHandler)
+	app.Post("/user", auth.SigninHandler)
 
 	//Save data to Database Routes
-	app.Post("/topic", requests.PostTopicHandler)
-	app.Post("/topic/research", requests.PostResearchHandler)
+	app.Post("/topic", handlers.PostTopicHandler)
+	app.Post("/topic/research", handlers.PostResearchHandler)
 
 	//Retrieve data from Database Routes
-	app.Post("/topics", requests.PostTopicsHandler)
-	app.Post("/topic/researches", requests.PostResearchesHandler)
+	app.Post("/topics", handlers.PostTopicsHandler)
+	app.Post("/topic/researches", handlers.PostResearchesHandler)
 
 }
