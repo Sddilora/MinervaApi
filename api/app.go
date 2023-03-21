be989a5db68d7d07a2a38b1c319a4b82456782a3
Sddilora <sumeyyedilaradogan@gmail.com>
2023-03-21 12:21:25 +0300

    middlewares moved into app.go

--- api/app.go
+++ api/app.go
@@ -1,18 +1,18 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
-	"time"
-
-	"context"
-
-	"github.com/gofiber/fiber/v2"
-
 	"minerva_api/api/routes"
+	"time"
 
 	firebase "firebase.google.com/go"
 	"firebase.google.com/go/auth"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	recoverMw "github.com/gofiber/fiber/v2/middleware/recover"
 	"google.golang.org/api/option"
 )
 
@@ -26,17 +26,23 @@ func main() {
 	//Creates new fiber app
 	app := fiber.New()
 
+	//Middlewares
+	app.Use(logger.New())    //records the details of incoming requests when any HTTP request is made. This can be used for purposes such as debugging and performance optimization.
+	app.Use(recoverMw.New()) //catches any errors that may cause the program to crash or interrupt and keep the server running.
+	app.Use(cors.New())      //It helps applications bypass CORS restrictions by providing appropriate responses that allow or deny HTTP requests access to their resources.
+
 	routes.ResearchRouter(app, appFire)
+	routes.TopicRouter(app, appFire)
 
 	defer cancel()
 	//Starts the HTTP server
-	log.Fatal(app.Listen(":7334"))
+	log.Fatal(app.Listen(":8080"))
 }
 
 func databaseConnection() (*auth.Client, *firebase.App, context.CancelFunc, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	//Take file and return as opt for API to use
-	opt := option.WithCredentialsFile("C:/Users/sumey/Desktop/software/Back-End/Minerva/FireBase/key.json")
+	opt := option.WithCredentialsFile("./key.json")
 	//Creates a new App from the provided config and client options.
 	appFire, err := firebase.NewApp(ctx, nil, opt)
 	if err != nil {
