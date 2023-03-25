7b7931abda3f6bb2080ffa50f63a358ea7ff4e96
Sddilora <sumeyyedilaradogan@gmail.com>
2023-03-25 22:49:42 +0300

    new route added for upload pdf

--- api/routes/research.go
+++ api/routes/research.go
@@ -4,14 +4,16 @@ import (
 	"minerva_api/api/handlers"
 
 	firebase "firebase.google.com/go"
+	storage "firebase.google.com/go/storage"
 	"github.com/gofiber/fiber/v2"
 )
 
 // ResearchRouter is the Router for GoFiber App
-func ResearchRouter(app *fiber.App, appFire *firebase.App) {
+func ResearchRouter(app *fiber.App, appFire *firebase.App, storageClient *storage.Client) {
 	app.Get("/topic/researches", handlers.GetResearches(appFire))
 	app.Get("/topic/research/:id", handlers.GetResearchByID(appFire))
 	app.Post("/topic/research", handlers.AddResearch(appFire))
 	app.Put("/topic/research", handlers.UpdateResearch(appFire))
 	app.Delete("/topic/research", handlers.RemoveResearch(appFire))
+	app.Post("/topic/research/pdf", handlers.AddPdf(storageClient))
 }
