7751abf7270394266dfb1bd56d6dedc3e9bf06eb
m-babur <baburakbas4646@gmail.com>
2023-03-26 06:37:30 +0300

    add new controller for file upload

--- api/routes/research.go
+++ api/routes/research.go
@@ -4,16 +4,14 @@ import (
 	"minerva_api/api/handlers"
 
 	firebase "firebase.google.com/go"
-	storage "firebase.google.com/go/storage"
 	"github.com/gofiber/fiber/v2"
 )
 
 // ResearchRouter is the Router for GoFiber App
-func ResearchRouter(app *fiber.App, appFire *firebase.App, storageClient *storage.Client) {
+func ResearchRouter(app *fiber.App, appFire *firebase.App) {
 	app.Get("/topic/researches", handlers.GetResearches(appFire))
 	app.Get("/topic/research/:id", handlers.GetResearchByID(appFire))
 	app.Post("/topic/research", handlers.AddResearch(appFire))
 	app.Put("/topic/research", handlers.UpdateResearch(appFire))
 	app.Delete("/topic/research", handlers.RemoveResearch(appFire))
-	app.Post("/topic/research/pdf", handlers.AddPdf(storageClient))
 }
